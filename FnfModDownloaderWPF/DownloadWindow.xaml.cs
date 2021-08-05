cee2f3399faf0cbe5821e1c3e0d58819924f30aa
hewo <rthongthipaya@gmail.com>
2021-08-05 18:14:24 -0400

    Add project files.

---
+++ FnfModDownloaderWPF/DownloadWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace FnfModDownloaderWPF
+{
+    /// <summary>
+    /// Interaction logic for DownloadWindow.xaml
+    /// </summary>
+    public partial class DownloadWindow : Window
+    {
+        public static bool ShowBytes = false;
+        public static bool ShowETA = false;
+        public static string path = "";
+        public static string name = "";
+        public DownloadWindow()
+        {
+            InitializeComponent();
+            Loaded += StartDownload;
+        }
+
+        private void StartDownload(object sender, RoutedEventArgs e)
+        {
+            StatusLbl.Content = "Downloading...";
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                path = "";
+                name = MainWindow.NameValue;
+                string extension = "";
+                path = path + MainWindow.DirValue;
+                if (MainWindow.ZipValue == true) { name = name + ".zip"; extension = "zip"; }
+                if (MainWindow.SevenZValue == true) { name = name + ".7z"; extension = "7z"; }
+                if (Directory.Exists("temp"))
+                {
+                    //Path exists already
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory("temp");
+                }
+                if (MainWindow.ZipValue == true && MainWindow.SevenZValue == true)
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(@"temp\" + MainWindow.NameValue);
+                    wc.DownloadFileAsync(new System.Uri(MainWindow.LinkValue), @"temp\" + MainWindow.NameValue);
+                }
+                else { wc.DownloadFileAsync(new System.Uri(MainWindow.LinkValue), @"temp\" + name); }
+                StreamWriter sw = new StreamWriter("Downloads.txt");
+                sw.WriteLine(MainWindow.DirValue + MainWindow.NameValue + "|" + MainWindow.LinkValue + "|" + MainWindow.NameValue + "|" + extension);
+                sw.Close();
+            }
+            
+            
+        }
+        // Event to track the progress
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = e.ProgressPercentage;
+            PercentLbl.Content = e.ProgressPercentage.ToString() + "%";
+            StatusLbl.Content = "Downloading...";
+            if (e.ProgressPercentage == 100)
+            {
+                StatusLbl.Content = "Creating directory...";
+                if (Directory.Exists(MainWindow.DirValue + MainWindow.NameValue))
+                {
+                    //Path already exists
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(MainWindow.DirValue + name);
+                }
+                PercentLbl.Content = "???";
+                if (MainWindow.ZipValue == true)
+                {
+                    if (MainWindow.Reinstall == true)
+                    {
+                        StatusLbl.Content = "Extracting...";
+                        Thread.Sleep(1000);
+                        ZipFile.ExtractToDirectory(@"temp\" + name, MainWindow.DirValue);
+                        StatusLbl.Content = "Deleting Zip file...";
+                        File.Delete(@"temp\" + name);
+                        Directory.Delete(MainWindow.DirValue + name);
+                        Process.Start("explorer.exe", MainWindow.DirValue);
+                        this.Close();
+                    }
+                    else
+                    {
+                         StatusLbl.Content = "Extracting...";
+                         Thread.Sleep(1000);
+                         ZipFile.ExtractToDirectory(@"temp\" + name, MainWindow.DirValue + MainWindow.NameValue);
+                         StatusLbl.Content = "Deleting Zip file...";
+                         File.Delete(@"temp\" + name);
+                         Directory.Delete(MainWindow.DirValue + name);
+                         Process.Start("explorer.exe", MainWindow.DirValue + MainWindow.NameValue);
+                         this.Close();
+                    }
+                   
+                }
+                if (MainWindow.SevenZValue == true)
+                {
+                    if (MainWindow.Reinstall == true)
+                    {
+                        StatusLbl.Content = "Extracting...";
+                        ExtractFile(@"temp\" + name, MainWindow.DirValue);
+                        StatusLbl.Content = "Deleting 7z file...";
+                        File.Delete(@"temp\" + name);
+                        Directory.Delete(MainWindow.DirValue + name);
+                        Process.Start("explorer.exe", MainWindow.DirValue);
+                        this.Close();
+                    }
+                    else
+                    {
+                        StatusLbl.Content = "Extracting...";
+                        ExtractFile(@"temp\" + name, MainWindow.DirValue + MainWindow.NameValue);
+                        StatusLbl.Content = "Deleting 7z file...";
+                        File.Delete(@"temp\" + name);
+                        Directory.Delete(MainWindow.DirValue + name);
+                        Process.Start("explorer.exe", MainWindow.DirValue + MainWindow.NameValue);
+                        this.Close();
+                    }
+                }
+                MainWindow.Reinstall = false;
+            }
+        }
+
+        public void ExtractFile(string sourceArchive, string destination)
+        {
+            string zPath = "7za.exe"; //add to proj and set CopyToOuputDir
+            try
+            {
+                ProcessStartInfo pro = new ProcessStartInfo();
+                pro.WindowStyle = ProcessWindowStyle.Hidden;
+                pro.FileName = zPath;
+                pro.Arguments = string.Format("x \"{0}\" -y -o\"{1}\"", sourceArchive, destination);
+                Process x = Process.Start(pro);
+                x.WaitForExit();
+            }
+            catch (System.Exception Ex)
+            {
+                //handle error
+            }
+        }
+    }
+
+}
+
---
+++ FnfModDownloaderWPF/ListWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FnfModDownloaderWPF
+{
+    /// <summary>
+    /// Interaction logic for ListWindow.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        public static string[] mods =
+        {
+            "The Full-Ass Tricky Mod|https://gamebanana.com/dl/583960|zip",
+            "B-Side Remixes|https://gamebanana.com/dl/554959|zip",
+            "Hatsune Miku|https://gamebanana.com/dl/534884|zip",
+            "Smoke 'Em Out Struggle (Garcello)|https://gamebanana.com/dl/563495|zip",
+            "Hex Mod|https://gamebanana.com/dl/535849|zip",
+            "Arcade Showdown (Kapi)|https://gamebanana.com/dl/580564|zip",
+            "Friday Night Funkin' Multiplayer|https://gamebanana.com/dl/542560|zip",
+            "Neo|https://gamebanana.com/dl/526052|zip",
+            "Tabi|https://gamebanana.com/dl/577675|zip",
+            "Agoti|https://gamebanana.com/dl/598687|zip",
+            "Zardy|https://gamebanana.com/dl/537613|zip",
+            "Shaggy|https://gamebanana.com/dl/579603|zip",
+            "Matt|https://gamebanana.com/dl/587470|zip",
+            "literally every fnf mod ever (Bob)|https://gamebanana.com/dl/611533|zip",
+            "Friday Night Funkin’ HD|https://gamebanana.com/dl/621798|zip",
+            "Friday Night Funkin, but bad|https://gamebanana.com/dl/529588|zip",
+            "Monika|https://gamebanana.com/dl/603297|zip",
+            "Imposter V2|https://gamebanana.com/dl/576560|zip",
+            "Carol V2|https://gamebanana.com/dl/541805|zip",
+            "Tord|https://gamebanana.com/dl/540211|zip",
+            "Starlight Mayhem (Cj)|https://gamebanana.com/dl/575291|zip",
+            "Fnf|https://www.mcpe117.com/funkin-windows-64bit.zip|zip",
+            "Autoplay Mod|https://gamebanana.com/dl/570755|7z"
+        };
+        public ListWindow()
+        {
+            InitializeComponent();
+            Loaded += MyWindow_Loaded;
+        }
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            StatusLbl.Content = "Loading...";
+            foreach(string text in mods)
+            {
+                ListBox.Items.Add(text.Split('|')[0]);
+            }
+            StatusLbl.Content = "Ready";
+        }
+        private void DownloadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLbl.Content = "Downloading...";
+            int Selected = ListBox.SelectedIndex;
+            if (Selected == -1)
+            {
+                MessageBox.Show("You need to select something to continue", "Can't continue!", MessageBoxButton.OK, MessageBoxImage.Error);
+                StatusLbl.Content = "Ready";
+                return;
+            }
+            MainWindow.NameValue = mods[Selected].Split('|')[0];
+            MainWindow.LinkValue = mods[Selected].Split('|')[1];
+            if (mods[Selected].Split('|')[2] == "zip") { MainWindow.ZipValue = true; }
+            if (mods[Selected].Split('|')[2] == "7z") { MainWindow.SevenZValue = true; }
+            DownloadWindow downloadWindow = new DownloadWindow();
+            downloadWindow.ShowDialog();
+            StatusLbl.Content = "Ready";
+        }
+    }
+}
